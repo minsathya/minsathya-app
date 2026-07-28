711efd84b1a57b72ba59fefccca97c32afb17046
sathya <kadaveerasatyasai2@gmail.com>
2026-07-28 05:36:12 +0530

    Update app icon, splash screen and app files

--- android/app/src/main/java/com/minsathya/app/MainActivity.java
+++ android/app/src/main/java/com/minsathya/app/MainActivity.java
@@ -1,5 +1,18 @@
 package com.minsathya.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     getBridge().getWebView().setBackgroundColor(
+    Color.parseColor("#0b0b0b")
+);
+
+    }
+}
